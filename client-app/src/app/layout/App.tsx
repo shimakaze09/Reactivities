430a0cacbc5e54132c8e245851f987cc1fd1dbf3
John <yangtianzhuo970425@gmail.com>
2023-11-17 11:10:33 +1300

    Adding routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,19 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../api/stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-
-        <ActivityDashboard />
+        <Route exact path="/" Component={HomePage} />
+        <Route path='/activities' Component={ActivityDashboard} />
+        <Route path='/createActivities' Component={ActivityForm} />
       </Container>
     </>
   );
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+function HomePage() {
+    return (
+        <Container style={{ marginTop: '7em' }}>
+            <h1>Home page</h1>
+        </Container>
+    )
+}
+
+export default HomePage;
