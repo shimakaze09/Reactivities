a0afafa47c47461f1355890183eab094db3a2b19
John <yangtianzhuo970425@gmail.com>
2023-11-18 12:00:57 +1300

    Creating a reusable text input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,6 +7,7 @@ import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
 
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
@@ -27,6 +28,11 @@ export default observer(function ActivityForm() {
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        venue: Yup.string().required(),
+        city: Yup.string().required(),
     })
 
     useEffect(() => {
@@ -54,14 +60,11 @@ export default observer(function ActivityForm() {
             <Formik validationSchema={validationSchema} enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <FormField>
-                            <Field placeholder='Title' name='title' />
-                            <ErrorMessage name='title' render={error => <Label basic color='red' content={error} />} />
-                        </FormField>
-                        <Field placeholder='Description' name='description' />
-                        <Field placeholder='Category' name='category' />
-                        <Field type='date' placeholder='Date' name='date' />
-                        <Field placeholder='Venue' name='venue' />
+                        <MyTextInput name='title' placeholder='Title' />
+                        <MyTextInput placeholder='Description' name='description' />
+                        <MyTextInput placeholder='Category' name='category' />
+                        <MyTextInput placeholder='Date' name='date' />
+                        <MyTextInput placeholder='Venue' name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
