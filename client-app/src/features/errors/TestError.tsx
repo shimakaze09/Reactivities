38158c63a1a4ec820763ef3d09faca996f80c6dc
John <yangtianzhuo970425@gmail.com>
2023-11-17 23:58:51 +1300

    Handling 400 errors

--- client-app/src/features/errors/TestError.tsx
+++ client-app/src/features/errors/TestError.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Header, Segment } from "semantic-ui-react";
 import axios from 'axios';
+import ValidationError from './ValidationError';
 
 export default function TestErrors() {
     const baseUrl = 'http://localhost:5000/api/';
@@ -27,7 +28,7 @@ export default function TestErrors() {
     }
 
     function handleValidationError() {
-        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err.response));
+        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err));
     }
 
     return (
@@ -43,6 +44,7 @@ export default function TestErrors() {
                     <Button onClick={handleBadGuid} content='Bad Guid' basic primary />
                 </Button.Group>
             </Segment>
+            {errors && <ValidationError errors={errors} />}
         </>
     )
 }
