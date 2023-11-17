54b36b22b6d94dd7e787192c7beabb8725749298
John <yangtianzhuo970425@gmail.com>
2023-11-17 21:43:04 +1300

    Handling API Error responses part 3

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,9 +7,9 @@ namespace API.Controllers;
 public class ActivitiesController : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities()
+    public async Task<IActionResult> GetActivities()
     {
-        return await Mediator.Send(new List.Query());
+        return HandleResult(await Mediator.Send(new List.Query()));
     }
 
     [HttpGet("{id:Guid}")]
@@ -23,7 +23,7 @@ public class ActivitiesController : BaseApiController
     [HttpPost]
     public async Task<IActionResult> CreateActivity(Activity activity)
     {
-        return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
     }
 
     [HttpPut("{id:Guid}")]
