0b0c434b6af4b00d24d7d4a970e957536db30fd8
John <yangtianzhuo970425@gmail.com>
2023-11-17 23:17:57 +1300

    Using an interceptor to handle API error responses

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddFluentValidation(config =>
-{
-    config.RegisterValidatorsFromAssemblyContaining<Create>();
-});
+builder.Services.AddControllers();
+
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
