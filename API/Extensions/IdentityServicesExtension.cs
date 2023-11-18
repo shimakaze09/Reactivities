88bbe3e67ef87a4ace9acf914a2bcf28316ee854
John <yangtianzhuo970425@gmail.com>
2023-11-18 20:53:55 +1300

    Storing secrets in development

--- API/Extensions/IdentityServicesExtension.cs
+++ API/Extensions/IdentityServicesExtension.cs
@@ -19,7 +19,7 @@ public static class IdentityServicesExtension
             .AddEntityFrameworkStores<DataContext>()
             .AddSignInManager<SignInManager<AppUser>>();
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("J1c2VyIjp7ImlkIjoxLCJuYW1lIjoiU2FtcGxlIEFsbGVuIn0sImV4cCI6MTcyNjY5MDM3Nn0.5643210aaf14a000656b7989210c7d32a871b552642f6126b436c3761a146e0"));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
 
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(opt =>
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,6 +8,13 @@ namespace API.Services;
 
 public class TokenService
 {
+    private readonly IConfiguration _config;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
     public string CreateToken(AppUser user)
     {
         var claims = new List<Claim>
@@ -17,7 +24,7 @@ public class TokenService
             new Claim(ClaimTypes.Email,user.Email),
         };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("J1c2VyIjp7ImlkIjoxLCJuYW1lIjoiU2FtcGxlIEFsbGVuIn0sImV4cCI6MTcyNjY5MDM3Nn0.5643210aaf14a000656b7989210c7d32a871b552642f6126b436c3761a146e0"));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
         var tokenDescriptor = new SecurityTokenDescriptor
