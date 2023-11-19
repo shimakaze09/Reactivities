3607923a193a348b29bbd99fe1bcec2f88b37e6f
John <yangtianzhuo970425@gmail.com>
2023-11-19 22:42:20 +1300

    Updating the home page and nav bar

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { User, UserFormValues } from "../models/user";
 import agent from "../api/agent";
+import { store } from "./store";
+import { router } from "../router/Routes";
 
 export default class UserStore {
     user: User | null = null;
@@ -9,16 +11,25 @@ export default class UserStore {
         makeAutoObservable(this)
     }
 
-    get isLoggedIn(): boolean {
+    get isLoggedIn() {
         return !!this.user;
     }
 
     login = async (creds: UserFormValues) => {
         try {
             const user = await agent.Account.login(creds);
-            console.log(user);
+            store.commonStore.setToken(user.token);
+            runInAction(() => this.user = user);
+            router.navigate('/activities');
         } catch (error) {
             throw error;
         }
+    }
+
+    logout = () => {
+        store.commonStore.setToken(null);
+        window.localStorage.removeItem('jwt');
+        this.user = null;
+        router.navigate('/');
     }
 }
--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
-import { Button, Label } from "semantic-ui-react";
+import { Button, Header, Label } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 
@@ -9,19 +9,20 @@ export default observer(function LoginForm() {
     return (
         <Formik
             initialValues={{ email: '', password: '', error: null }}
-            onSubmit={(values, { setErrors }) => userStore.login(values).catch(error => setErrors({ error: 'Invalid email or password' }))}
+            onSubmit={(values, { setErrors }) =>
+                userStore.login(values).catch(error => setErrors({ error: 'Invalid email or password' }))}
         >
             {({ handleSubmit, isSubmitting, errors }) => (
-                <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-                    <MyTextInput name="email" placeholder="Email" />
-                    <MyTextInput name="password" placeholder="Password" type='password' />
-                    <ErrorMessage
-                        name="error"
-                        render={() => <Label style={{ marginBottom: 10 }} basic color="red" content={errors.error} />}
-                    />
+                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                    <Header as='h2' content='Login to Reactivities' color="teal" textAlign="center" />
+                    <MyTextInput placeholder="Email" name='email' />
+                    <MyTextInput placeholder="Password" name='password' type='password' />
+                    <ErrorMessage name='error' render={() =>
+                        <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />} />
                     <Button loading={isSubmitting} positive content='Login' type="submit" fluid />
                 </Form>
             )}
-        </Formik >
+
+        </Formik>
     )
 })
