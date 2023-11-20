5801503394a76a0ba1fae40a48275a0d8d3731c5
John <yangtianzhuo970425@gmail.com>
2023-11-20 15:06:01 +1300

    Shaping the related data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,25 +9,31 @@ namespace Application.Activities;
 
 public class List
 {
-    public class Query : IRequest<Result<List<Activity>>> { }
+    public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-    public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+    public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
     {
         private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-        public Handler(DataContext context)
+
+        public Handler(DataContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
+
         }
 
-        public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken token)
+        public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken token)
         {
             var activities = await _context.Activities
                 .Include(a => a.Attendees)
                 .ThenInclude(u => u.AppUser)
                 .ToListAsync();
 
-            return Result<List<Activity>>.Success(activities);
+            var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+
+            return Result<List<ActivityDto>>.Success(activitiesToReturn);
         }
     }
 }
