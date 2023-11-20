d4b5630f3635c2fb5a9857fc097335dc38d1d7d2
John <yangtianzhuo970425@gmail.com>
2023-11-20 14:00:31 +1300

    Configuring the new relationship

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Domain;
 
-public class AppUser: IdentityUser
+public class AppUser : IdentityUser
 {
     public string DisplayName { get; set; }
     public string Bio { get; set; }
+    public ICollection<ActivityAttendee> Activities { get; set; }
 }
