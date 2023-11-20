b0d86d0ed80ca9c3cf3f561e70db1653ad55af69
John <yangtianzhuo970425@gmail.com>
2023-11-21 12:23:38 +1300

    Adding the Photo entity

---
+++ Persistence/Migrations/20231120232153_PhotoEntityAdded.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotoEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_activityAttendees_Activities_ActivityId",
+                table: "activityAttendees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_activityAttendees_AspNetUsers_AppUserId",
+                table: "activityAttendees");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_activityAttendees",
+                table: "activityAttendees");
+
+            migrationBuilder.RenameTable(
+                name: "activityAttendees",
+                newName: "ActivityAttendees");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_activityAttendees_ActivityId",
+                table: "ActivityAttendees",
+                newName: "IX_ActivityAttendees_ActivityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityAttendees",
+                table: "ActivityAttendees",
+                columns: new[] { "AppUserId", "ActivityId" });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: true),
+                    IsMain = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AppUserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photos",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActivityAttendees_Activities_ActivityId",
+                table: "ActivityAttendees",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActivityAttendees_AspNetUsers_AppUserId",
+                table: "ActivityAttendees",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActivityAttendees_Activities_ActivityId",
+                table: "ActivityAttendees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActivityAttendees_AspNetUsers_AppUserId",
+                table: "ActivityAttendees");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityAttendees",
+                table: "ActivityAttendees");
+
+            migrationBuilder.RenameTable(
+                name: "ActivityAttendees",
+                newName: "activityAttendees");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ActivityAttendees_ActivityId",
+                table: "activityAttendees",
+                newName: "IX_activityAttendees_ActivityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_activityAttendees",
+                table: "activityAttendees",
+                columns: new[] { "AppUserId", "ActivityId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_activityAttendees_Activities_ActivityId",
+                table: "activityAttendees",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_activityAttendees_AspNetUsers_AppUserId",
+                table: "activityAttendees",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
