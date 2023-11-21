ce0e457b2039b67912f4bb7f1886cb59bb929c4a
John <yangtianzhuo970425@gmail.com>
2023-11-22 12:33:23 +1300

    Adding a Dropzone

---
+++ client-app/src/app/common/ImageUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+function PhotoWidgetDropzone() {
+    const onDrop = useCallback(() => {
+
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the files here...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+        </div>
+    )
+}
+
+export default PhotoWidgetDropzone;
